3e1979ae66e006d980e3e422c22ead1025dec2da
Daria Caraway <caraway@usc.edu>
2014-04-06 11:53:37 -0700

    adding cache feature to api calls

---
+++ app/config/local/flickr.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daria
+ * Date: 4/6/14
+ * Time: 11:18 AM
+ */
+return [
+    'api_key' => 'c9a47e4f7820988abba5bd83283b1115',
+];
--- app/controllers/FlickrController.php
+++ app/controllers/FlickrController.php
@@ -9,10 +9,21 @@ class FlickrController extends BaseController {
     }
 
     public function getResults(){
-        $tag = Input::get('tag');
-        $flickr = new \Itp\API\FlickrSearch($tag);
-        $json = $flickr->getResults();
-        $results = $json->photos->photo;
+        $results = Cache::get('flickr-itp');
+
+        if(!$results){
+            $tag = Input::get('tag');
+            $flickr = new \Itp\API\FlickrSearch($tag,[
+                'api_key' => Config::get('flickr.api_key')
+            ]);
+            $json = $flickr->getResults();
+            $results = $json->photos->photo;
+
+            Cache::put('flickr-itp', $results, .02);
+
+        }
+
+        //var_dump($results);
 
         return View::make("flickr/results", [
             'results' => $results
